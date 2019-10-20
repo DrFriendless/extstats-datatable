f03cf61e2b3c2e00f5386e71ed131623f8e3ff20
DrFriendless <Borod1n>
2019-10-20 13:51:38 +1100

    v2.0.2

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-export {DataTable, DataEvent, PageEvent, SortEvent} from './lib/src/DataTable';
+export {DataTable, PageEvent, SortEvent} from './lib/src/DataTable';
 export {DefaultSorter} from './lib/src/DefaultSorter';
 export {Paginator} from './lib/src/Paginator';
 export {BootstrapPaginator} from './lib/src/BootstrapPaginator';
